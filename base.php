02f589954350f42c3061839a1a8c595aedf6d691
mahdikhodsiani <45894288+mahdikhodsiani@users.noreply.github.com>
2021-06-11 20:42:48 +0430

    Add files via upload

---
+++ base.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title> صفحه اصلی </title>
+      
+        <link rel="stylesheet" href="public/assets/css/m.css">
+        <link rel="stylesheet" href="public/assets/css/base.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Neuton|Oswald'><link rel="stylesheet" href="css/styl.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+        
+		
+    </head>
+    <body class="container vazirfont" > 
+		
+    <nav lang="fa"  class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <!-- Brand/logo -->
+        <a class="navbar-brand" href="public/index.php">
+            <img src="public/assets/images/logo.png" alt="logo" style="width:40px;">
+        </a>
+        
+        <!-- Links -->
+        <ul  class=" navbar-nav">
+            
+			<li class="nav-item">
+            <a class="nav-link" href="tahlil.php" target="_blank">تحلیل های من</a>
+            </li> 			
+            <li class="nav-item">
+            <a class="nav-link" href="http://mahdikhodsiani.b6b.ir/search/" target="_blank">سایت من</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="public/contact.php">تماس با من</a>
+            </li>
+            <a style="position: absolute; z-index: 4; left: 10px; " href="public/signin.php" class="btn btn-success" role="button"> ثبت نام </a>
+        </ul>
+    </nav>
+	
+			       
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+     
+        <link rel="stylesheet" href="style.css">
+       
+<!-- partial:index.partial.html -->
+<p>
+  <span>
+    به سایت مهدی خودسیانی خوش آمدید
+  </span>
+ <b><font size="6" color="yellow">     از منوی بالا برای ادامه کار استفاده کنید   </b></font>
+</p>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  		
+    </body>
+	   </html>
